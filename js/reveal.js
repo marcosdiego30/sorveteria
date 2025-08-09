bea30ff838ca34e8bb78b21f12d38ecd6bd4a52c
marcosdiego30 <marcosdev250@gmail.com>
2025-08-08 23:17:03 -0300

    mudei o title, adicionei class e efeito de scroll

---
+++ js/reveal.js
@@ -0,0 +1,68 @@
+window.revelar = ScrollReveal({'reset': true});
+
+revelar.reveal('.efeito-shape', {
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-cta', {
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-banner', {
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito', {
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-title2', {
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+});
+
+revelar.reveal('.efeito2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'left'
+});
+
+revelar.reveal('.efeito3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'reight'
+});
+
+revelar.reveal('.efeito4', {
+    duration: 2000,
+    distance: '90px',
+    delay: 2000,
+    origin: 'reight'
+});
+
+revelar.reveal('.efeito-img', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+});
+
+revelar.reveal('.efeito-testimunhos', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'reight'
+});
